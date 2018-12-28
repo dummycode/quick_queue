595b371bd38646bac77f9cda79e1fb9532951a72
Henry Harris <henrythomasharris@gmail.com>
2018-12-28 11:26:44 -0500

    Add lint

---
+++ server/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            { SwitchCase: 1 }
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
